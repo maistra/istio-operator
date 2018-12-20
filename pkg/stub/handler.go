2aa38e08ec8124ab9408f52fbd70758bc5842c54
Kevin Conner <kconner@redhat.com>
2018-12-20 05:56:26 -0800

    Updated operator to support maistra-0.6.0: MAISTRA-139

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -28,7 +28,7 @@ const (
 	configurationDir = "/etc/origin/master"
 
 	defaultIstioPrefix = "docker.io/maistra/"
-	defaultIstioVersion = "0.5.0"
+	defaultIstioVersion = "0.6.0"
 	defaultDeploymentType = "origin"
 
 	newline = "\n"
