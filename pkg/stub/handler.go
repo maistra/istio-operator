8d12af7fb22071fd1c93189d686b5c099739d5fb
Kevin Conner <kconner@redhat.com>
2019-01-17 14:08:26 -0800

    Updated for maistra 0.7.0: MAISTRA-161

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -28,7 +28,7 @@ const (
 	configurationDir = "/etc/origin/master"
 
 	defaultIstioPrefix = "docker.io/maistra/"
-	defaultIstioVersion = "0.6.0"
+	defaultIstioVersion = "0.7.0"
 	defaultDeploymentType = "origin"
 
 	newline = "\n"
