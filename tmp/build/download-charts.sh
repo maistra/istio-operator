7d10ff268655fe336909b66bd803bb1e767f10d9
rcernich <rcernich@redhat.com>
2019-04-11 19:12:28 -0600

    updates for sidecar patches and 3scale version
    Signed-off-by: rcernich <rcernich@redhat.com>

--- tmp/build/download-charts.sh
+++ tmp/build/download-charts.sh
@@ -3,6 +3,7 @@
 set -e
 
 : ${MAISTRA_VERSION:=0.10.0}
+: ${MAISTRA_BRANCH:=0.10}
 
 SOURCE_DIR=$(pwd)
 DIR=$(pwd)/tmp/_output/helm
@@ -13,19 +14,12 @@ ISTIO_VERSION=1.1.0
 RELEASES_DIR=${DIR}/istio-releases
 
 PLATFORM=linux
-if [ -n "${ISTIO_VERSION}" ] ; then
-  ISTIO_NAME=istio-${ISTIO_VERSION}
-  ISTIO_FILE="${ISTIO_NAME}-${PLATFORM}.tar.gz"
-  ISTIO_URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/${ISTIO_FILE}"
-  EXTRACT_CMD="tar --strip-components=4 -C ./${ISTIO_NAME} -xvzf ${ISTIO_FILE} ${ISTIO_NAME}/install/kubernetes/helm"
-  RELEASE_DIR="${RELEASES_DIR}/${ISTIO_NAME}"
-else
-  ISTIO_NAME=istio-${ISTIO_BRANCH}
-  ISTIO_FILE="${ISTIO_BRANCH}.zip"
-  ISTIO_URL="https://github.com/istio/istio/archive/${ISTIO_FILE}"
-  EXTRACT_CMD="unzip ${ISTIO_FILE} ${ISTIO_NAME}/install/kubernetes/helm"
-  RELEASE_DIR="${RELEASES_DIR}/${ISTIO_NAME}"
-fi
+
+ISTIO_NAME=istio-${ISTIO_VERSION}
+ISTIO_FILE="maistra-${MAISTRA_BRANCH}.zip"
+ISTIO_URL="https://github.com/Maistra/istio/archive/maistra-${MAISTRA_BRANCH}.zip"
+EXTRACT_CMD="unzip ${ISTIO_FILE} istio-maistra-${MAISTRA_BRANCH}/install/kubernetes/helm/*"
+RELEASE_DIR="${RELEASES_DIR}/${ISTIO_NAME}"
 
 ISTIO_NAME=${ISTIO_NAME//./-}
 
@@ -55,6 +49,7 @@ function retrieveIstioRelease() {
       echo "extracting Istio Helm charts to ${RELEASES_DIR}"
       cd "${RELEASES_DIR}"
       ${EXTRACT_CMD}
+      mv istio-maistra-${MAISTRA_BRANCH}/install/kubernetes/helm/* ${RELEASE_DIR}
       #(
       #  cd "${HELM_DIR}/istio"
       #  helm dep update
