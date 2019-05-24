0702e789a72d54531ac31cf82b7244366bbb5ba8
Marko Lukša <marko.luksa@gmail.com>
2019-05-24 18:33:05 +0200

    Make operator logging configurable MAISTRA-442

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -46,13 +46,16 @@ func main() {
 	// ControlPlane
 	flag.StringVar(&controlplane.ChartPath, "chartPath", "/etc/istio-operator/1.1.0/helm", "The location of the Helm charts.  The charts will be rendered using $chartPath/istio (similar layout to istio.io/istio/install/kubernetes/helm).")
 
+	logConfig := "production"
+	flag.StringVar(&logConfig, "logConfig", logConfig, "Whether to configure logging for production use (json, info level, w/ log sampling) or development (plain-text, debug level, w/o log sampling)")
+
 	flag.Parse()
 
 	// The logger instantiated here can be changed to any logger
 	// implementing the logr.Logger interface. This logger will
 	// be propagated through the whole operator, generating
 	// uniform and structured logs.
-	logf.SetLogger(logf.ZapLogger(false))
+	logf.SetLogger(logf.ZapLogger(logConfig != "production"))
 
 	printVersion()
 
