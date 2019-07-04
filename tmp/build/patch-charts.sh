3e5c63bbb3406a64a0b07cf1f3b70d220a9f170b
Jonh Wendell <jonh.wendell@redhat.com>
2019-07-04 12:58:44 -0400

    Use 'maistra-control-plane' for the label name

--- tmp/build/patch-charts.sh
+++ tmp/build/patch-charts.sh
@@ -152,9 +152,9 @@ function patchTemplates() {
     xargs sed -i -e 's/^\(.*\)release:\(.*\)$/\1maistra-version: '${MAISTRA_VERSION}'\
 \1release:\2/'
 
-# MAISTRA-506 add a namespace label for deployment specs
+# MAISTRA-506 add a maistra-control-plane label for deployment specs
 find ${HELM_DIR} -name "*.yaml" -o -name "*.yaml.tpl" | xargs grep -Hl 'kind: Deployment' |\
-  xargs sed -i -e '/^spec:/,$ { /template:$/,$ { /metadata:$/,$ { /labels:$/,$ s/^\(.*\)release:\(.*\)$/\1namespace: {{ .Release.Namespace }}\n\1release:\2/ } } }'
+  xargs sed -i -e '/^spec:/,$ { /template:$/,$ { /metadata:$/,$ { /labels:$/,$ s/^\(.*\)release:\(.*\)$/\1maistra-control-plane: {{ .Release.Namespace }}\n\1release:\2/ } } }'
 
   # update the images
   # set global.hub=docker.io/maistra
