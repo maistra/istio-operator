4098d29b273d852a591db3e37c2fb9ae8799fb89
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2019-08-13 13:59:09 -0700

    MAISTRA-411 renamed product OLM metadata to servicemesh

--- tmp/build/generate-manifests.sh
+++ tmp/build/generate-manifests.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
-# to generate Maistra OLM metadata: MAISTRA_VERSION=1.0.0 DEPLOYMENT_FILE=deploy/maistra-operator.yaml MANIFESTS_DIR=manifests-maistra tmp/build/generate-manifests.sh
-# to generate ServiceMesh OLM metadata: MAISTRA_VERSION=1.0.0 DEPLOYMENT_FILE=deploy/servicemesh-operator.yaml MANIFESTS_DIR=manifests-servicemesh tmp/build/generate-manifests.sh
+# to generate Maistra OLM metadata: MAISTRA_VERSION=1.0.0 tmp/build/generate-manifests.sh
+# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.0.0 tmp/build/generate-manifests.sh
 
 set -e
 
-: ${DEPLOYMENT_FILE:?"Need to set location of the source deployment file, e.g. deploy/maistra-operator.yaml"}
-: ${MANIFESTS_DIR:?"Need to set location of the manifests directory, e.g. manifests/"}
+: ${COMMUNITY:-"true"}
 : ${MAISTRA_VERSION:?"Need to set maistra version, e.g. 1.0.0"}
 
+if [[ ${COMMUNITY} == "true" ]]; then
+  BUILD_TYPE="maistra"
+else
+  BUILD_TYPE="servicemesh"
+fi
+: ${DEPLOYMENT_FILE:=deploy/${BUILD_TYPE}-operator.yaml}
+: ${MANIFESTS_DIR:=manifests-${BUILD_TYPE}}
 BUNDLE_DIR=${MANIFESTS_DIR}/${MAISTRA_VERSION}
+OPERATOR_NAME=${BUILD_TYPE}operator
 MY_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p "$BUNDLE_DIR"
@@ -52,9 +59,10 @@ function generateCSV() {
      exit 1
   fi
 
-  local csv_path=${BUNDLE_DIR}/maistra.v${MAISTRA_VERSION}.clusterserviceversion.yaml
+  local csv_path=${BUNDLE_DIR}/${OPERATOR_NAME}.v${MAISTRA_VERSION}.clusterserviceversion.yaml
   cp ${MY_LOCATION}/manifest-templates/clusterserviceversion.yaml ${csv_path}
 
+  sed -i -e 's/__NAME__/'${OPERATOR_NAME}'/g' ${csv_path}
   sed -i -e 's/__VERSION__/'${MAISTRA_VERSION}'/g' ${csv_path}
   sed -i -e 's/__DATE__/'$(date +%Y-%m-%dT%H:%M:%S%Z)'/g' ${csv_path}
   sed -i -e 's+__IMAGE_SRC__+'${IMAGE_SRC}'+g' ${csv_path}
@@ -69,8 +77,9 @@ function generateCSV() {
 }
 
 function generatePackage() {
-  local package_path=${MANIFESTS_DIR}/maistra.package.yaml
+  local package_path=${MANIFESTS_DIR}/${BUILD_TYPE}.package.yaml
   cp ${MY_LOCATION}/manifest-templates/package.yaml ${package_path}
+  sed -i -e 's/__NAME__/'${OPERATOR_NAME}'/g' ${package_path}
   sed -i -e 's/__VERSION__/'${MAISTRA_VERSION}'/g' ${package_path}
 }
 
