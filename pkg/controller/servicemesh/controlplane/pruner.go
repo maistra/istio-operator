8e6966ce0edce5e8514fac4971141546ee3295a4
Marko Lukša <marko.luksa@gmail.com>
2019-08-21 09:14:06 +0200

    MAISTRA-784 Don't log error if resource disappears during deletion
    If you delete the control plane namespace, the operator and OpenShift
    race to delete resources in that namespace. When the operator tries to
    delete an object, it shouldn't log an error if OpenShift has deleted it
    between the time the operator listed objects and then tried to delete
    them.

--- pkg/controller/servicemesh/controlplane/pruner.go
+++ pkg/controller/servicemesh/controlplane/pruner.go
@@ -107,7 +107,7 @@ func (r *ControlPlaneReconciler) pruneResources(gvks []schema.GroupVersionKind, 
 			if generation, ok := common.GetAnnotation(&object, common.MeshGenerationKey); ok && generation != instanceGeneration {
 				r.Log.Info("pruning resource", "resource", v1.NewResourceKey(&object, &object))
 				err = r.Client.Delete(context.TODO(), &object, client.PropagationPolicy(metav1.DeletePropagationBackground))
-				if err != nil {
+				if err != nil && !errors.IsNotFound(err) {
 					r.Log.Error(err, "Error pruning resource", "resource", v1.NewResourceKey(&object, &object))
 					allErrors = append(allErrors, err)
 				} else {
