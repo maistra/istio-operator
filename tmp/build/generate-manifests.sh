9d412d5cc289c76c9c9ef09480e616da6a9ecbfe
Kevin Conner <kconner@redhat.com>
2020-01-28 18:27:52 -0800

    MAISTRA-1136: update operator support for maistra 1.0.6

--- tmp/build/generate-manifests.sh
+++ tmp/build/generate-manifests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# to generate Maistra OLM metadata: MAISTRA_VERSION=1.0.5 REPLACES_CSV=1.0.4 tmp/build/generate-manifests.sh
-# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.0.5 REPLACES_CSV=1.0.4 tmp/build/generate-manifests.sh
+# to generate Maistra OLM metadata: MAISTRA_VERSION=1.0.6 REPLACES_CSV=1.0.5 tmp/build/generate-manifests.sh
+# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.0.6 REPLACES_CSV=1.0.5 tmp/build/generate-manifests.sh
 
 set -e
 
 COMMUNITY=${COMMUNITY:-"true"}
-: ${MAISTRA_VERSION:?"Need to set maistra version, e.g. 1.0.5"}
+: ${MAISTRA_VERSION:?"Need to set maistra version, e.g. 1.0.6"}
 if [[ ${COMMUNITY} == "true" ]]; then
   BUILD_TYPE="maistra"
   JAEGER_TEMPLATE="all-in-one"
