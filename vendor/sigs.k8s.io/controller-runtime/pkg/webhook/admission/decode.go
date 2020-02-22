65fe39d6dbbd20d7fc8b9c730a155c131dd60a9c
rcernich <rcernich@redhat.com>
2020-02-21 17:43:43 -0700

    update vendor for operator-sdk v0.15.2
    Signed-off-by: rcernich <rcernich@redhat.com>

--- vendor/sigs.k8s.io/controller-runtime/pkg/webhook/admission/decode.go
+++ vendor/sigs.k8s.io/controller-runtime/pkg/webhook/admission/decode.go
@@ -19,32 +19,58 @@ package admission
 import (
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/types"
+	"k8s.io/apimachinery/pkg/util/json"
 )
 
-type decoder struct {
+// Decoder knows how to decode the contents of an admission
+// request into a concrete object.
+type Decoder struct {
 	codecs serializer.CodecFactory
 }
 
 // NewDecoder creates a Decoder given the runtime.Scheme
-func NewDecoder(scheme *runtime.Scheme) (types.Decoder, error) {
-	return decoder{codecs: serializer.NewCodecFactory(scheme)}, nil
+func NewDecoder(scheme *runtime.Scheme) (*Decoder, error) {
+	return &Decoder{codecs: serializer.NewCodecFactory(scheme)}, nil
 }
 
 // Decode decodes the inlined object in the AdmissionRequest into the passed-in runtime.Object.
-func (d decoder) Decode(req types.Request, into runtime.Object) error {
-	return d.DecodeRaw(req.AdmissionRequest.Object, into)
+// If you want decode the OldObject in the AdmissionRequest, use DecodeRaw.
+// It errors out if req.Object.Raw is empty i.e. containing 0 raw bytes.
+func (d *Decoder) Decode(req Request, into runtime.Object) error {
+	// we error out if rawObj is an empty object.
+	if len(req.Object.Raw) == 0 {
+		return fmt.Errorf("there is no content to decode")
+	}
+	return d.DecodeRaw(req.Object, into)
 }
 
 // DecodeRaw decodes a RawExtension object into the passed-in runtime.Object.
 // It errors out if rawObj is empty i.e. containing 0 raw bytes.
-func (d decoder) DecodeRaw(rawObj runtime.RawExtension, into runtime.Object) error {
+func (d *Decoder) DecodeRaw(rawObj runtime.RawExtension, into runtime.Object) error {
+	// NB(directxman12): there's a bug/weird interaction between decoders and
+	// the API server where the API server doesn't send a GVK on the embedded
+	// objects, which means the unstructured decoder refuses to decode.  It
+	// also means we can't pass the unstructured directly in, since it'll try
+	// and call unstructured's special Unmarshal implementation, which calls
+	// back into that same decoder :-/
+	// See kubernetes/kubernetes#74373.
+
 	// we error out if rawObj is an empty object.
 	if len(rawObj.Raw) == 0 {
 		return fmt.Errorf("there is no content to decode")
 	}
+	if unstructuredInto, isUnstructured := into.(*unstructured.Unstructured); isUnstructured {
+		// unmarshal into unstructured's underlying object to avoid calling the decoder
+		if err := json.Unmarshal(rawObj.Raw, &unstructuredInto.Object); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
 	deserializer := d.codecs.UniversalDeserializer()
 	return runtime.DecodeInto(deserializer, rawObj.Raw, into)
 }
