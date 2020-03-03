b807b6d1e219ad01fc03248e221a0a99fca40930
Marko Lukša <marko.luksa@gmail.com>
2020-03-03 16:09:58 +0100

    MAISTRA-1204 Use local cache to check status of SMCP (#357)

--- pkg/controller/servicemesh/controlplane/readiness.go
+++ pkg/controller/servicemesh/controlplane/readiness.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -12,8 +14,6 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 const statusAnnotationReadyComponentCount = "readyComponentCount"
@@ -98,15 +98,31 @@ func (r *controlPlaneInstanceReconciler) updateReadinessStatus(ctx context.Conte
 	return updateStatus, nil
 }
 
+type isReadyFunc func(runtime.Object) bool
+
 func (r *controlPlaneInstanceReconciler) calculateComponentReadiness(ctx context.Context) (map[string]bool, error) {
 	readinessMap := map[string]bool{}
-	typesToCheck := map[schema.GroupVersionKind]func(context.Context, *unstructured.Unstructured) bool{
-		appsv1.SchemeGroupVersion.WithKind("Deployment"):  r.deploymentReady,
-		appsv1.SchemeGroupVersion.WithKind("StatefulSet"): r.statefulSetReady,
-		appsv1.SchemeGroupVersion.WithKind("DaemonSet"):   r.daemonSetReady,
+	typesToCheck := map[runtime.Object]isReadyFunc{
+		&appsv1.DeploymentList{}: func(obj runtime.Object) bool {
+			deployment := obj.(*appsv1.Deployment)
+			for _, condition := range deployment.Status.Conditions {
+				if condition.Type == appsv1.DeploymentAvailable {
+					return condition.Status == corev1.ConditionTrue
+				}
+			}
+			return false
+		},
+		&appsv1.StatefulSetList{}: func(obj runtime.Object) bool {
+			statefulSet := obj.(*appsv1.StatefulSet)
+			return statefulSet.Status.ReadyReplicas >= statefulSet.Status.Replicas
+		},
+		&appsv1.DaemonSetList{}: func(obj runtime.Object) bool {
+			daemonSet := obj.(*appsv1.DaemonSet)
+			return r.daemonSetReady(daemonSet)
+		},
 	}
-	for gvk, isReadyFunc := range typesToCheck {
-		err := r.calculateReadinessForType(ctx, gvk, readinessMap, isReadyFunc)
+	for list, readyFunc := range typesToCheck {
+		err := r.calculateReadinessForType(ctx, list, readyFunc, readinessMap)
 		if err != nil {
 			return readinessMap, err
 		}
@@ -122,98 +138,49 @@ func (r *controlPlaneInstanceReconciler) isCNIReady(ctx context.Context) (bool, 
 		return true, nil
 	}
 	labelSelector := map[string]string{"istio": "cni"}
-	daemonSets := &unstructured.UnstructuredList{}
-	daemonSets.SetGroupVersionKind(appsv1.SchemeGroupVersion.WithKind("DaemonSet"))
+	daemonSets := &appsv1.DaemonSetList{}
 	operatorNamespace := common.GetOperatorNamespace()
 	if err := r.Client.List(ctx, client.MatchingLabels(labelSelector).InNamespace(operatorNamespace), daemonSets); err != nil {
 		return false, err
 	}
 	for _, ds := range daemonSets.Items {
-		if !r.daemonSetReady(ctx, &ds) {
+		if !r.daemonSetReady(&ds) {
 			return false, nil
 		}
 	}
 	return true, nil
 }
 
-func (r *controlPlaneInstanceReconciler) calculateReadinessForType(ctx context.Context, gvk schema.GroupVersionKind, readinessMap map[string]bool, isReady func(context.Context, *unstructured.Unstructured) bool) error {
+func (r *controlPlaneInstanceReconciler) calculateReadinessForType(ctx context.Context, list runtime.Object, isReady isReadyFunc, readinessMap map[string]bool) error {
 	log := common.LogFromContext(ctx)
-	resources, err := common.FetchOwnedResources(ctx, r.Client, gvk, r.Instance.GetNamespace(), r.Instance.GetNamespace())
+
+	meshNamespace := r.Instance.GetNamespace()
+	selector := map[string]string{common.OwnerKey: meshNamespace}
+	err := r.Client.List(ctx, client.InNamespace(meshNamespace).MatchingLabels(selector), list)
 	if err != nil {
 		return err
 	}
-	for _, resource := range resources.Items {
-		if component, ok := common.GetLabel(&resource, common.KubernetesAppComponentKey); ok {
-			componentReady := isReady(ctx, &resource)
-			if ready, exists := readinessMap[component]; exists {
-				readinessMap[component] = ready && componentReady
-			} else {
-				readinessMap[component] = componentReady
-			}
+	items, err := meta.ExtractList(list)
+	if err != nil {
+		return err
+	}
+
+	for _, obj := range items {
+		metaObject, err := meta.Accessor(obj)
+		if err != nil {
+			return err
+		}
+		if component, ok := metaObject.GetLabels()[common.KubernetesAppComponentKey]; ok {
+			ready, exists := readinessMap[component]
+			readinessMap[component] = (ready || !exists) && isReady(obj)
 		} else {
 			// how do we have an owned resource with no component label?
-			log.Error(nil, "skipping resource for readiness check: resource has no component label", gvk.Kind, resource.GetName())
+			log.Error(nil, "skipping resource for readiness check: resource has no component label", obj.GetObjectKind(), metaObject.GetName())
 		}
 	}
 	return nil
 }
 
-func (r *controlPlaneInstanceReconciler) deploymentReady(ctx context.Context, deployment *unstructured.Unstructured) bool {
-	log := common.LogFromContext(ctx)
-	conditions, found, err := unstructured.NestedSlice(deployment.UnstructuredContent(), "status", "conditions")
-	if err != nil {
-		log.Error(err, "error reading Deployment.Status", "Deployment", deployment.GetName())
-		return false
-	}
-	if !found {
-		return false
-	}
-
-	for _, condition := range conditions {
-		if conditionMap, ok := condition.(map[string]interface{}); ok {
-			conditionType, _, _ := unstructured.NestedString(conditionMap, "type")
-			if conditionType == "Available" {
-				conditionStatus, _, _ := unstructured.NestedString(conditionMap, "status")
-				return conditionStatus == "True"
-			}
-		} else {
-			log.Error(nil, "cannot convert Deployment condition")
-		}
-	}
-
-	return false
-}
-
-func (r *controlPlaneInstanceReconciler) statefulSetReady(ctx context.Context, statefulSet *unstructured.Unstructured) bool {
-	log := common.LogFromContext(ctx)
-	replicas, found, err := unstructured.NestedInt64(statefulSet.UnstructuredContent(), "status", "replicas")
-	if err != nil {
-		log.Error(err, "error reading StatefulSet.Status", "StatefulSet", statefulSet.GetName())
-		return false
-	}
-	if !found {
-		return false
-	}
-
-	readyReplicas, found, err := unstructured.NestedInt64(statefulSet.UnstructuredContent(), "status", "readyReplicas")
-	if err != nil {
-		log.Error(err, "error reading StatefulSet.Status", "StatefulSet", statefulSet.GetName())
-		return false
-	}
-	if !found {
-		return false
-	}
-
-	return readyReplicas >= replicas
-}
-
-func (r *controlPlaneInstanceReconciler) daemonSetReady(ctx context.Context, daemonSet *unstructured.Unstructured) bool {
-	log := common.LogFromContext(ctx)
-	unavailable, found, err := unstructured.NestedInt64(daemonSet.UnstructuredContent(), "status", "numberUnavailable")
-	if err != nil {
-		log.Error(err, "error reading DaemonSet.Status", "DaemonSet", daemonSet.GetName())
-		return false
-	}
-
-	return !found || unavailable == 0
+func (r *controlPlaneInstanceReconciler) daemonSetReady(ds *appsv1.DaemonSet) bool {
+	return ds.Status.NumberUnavailable == 0
 }
