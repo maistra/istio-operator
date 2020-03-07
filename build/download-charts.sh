a8a3307621ed9ef81630b5df8cab97961944c63d
Marko Lukša <marko.luksa@gmail.com>
2020-03-07 09:04:06 +0100

    MAISTRA-1251 Allow overriding Maistra Istio repo in download-charts.sh using env var (#364)
    * MAISTRA-1251 Allow overriding Maistra Istio repo in download-charts.sh using env var
    
    * Extract EXTRACT_DIR var
    
    * Proper handling of repos with names other than "istio"
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- build/download-charts.sh
+++ build/download-charts.sh
@@ -3,6 +3,7 @@
 set -e
 
 : ${MAISTRA_VERSION:=1.1.0}
+: ${MAISTRA_REPO:=https://github.com/Maistra/istio}
 : ${MAISTRA_BRANCH:=maistra-1.1}
 
 : ${SOURCE_DIR:=$(pwd)}
@@ -17,8 +18,9 @@ PLATFORM=linux
 
 ISTIO_NAME=istio-${ISTIO_VERSION}
 ISTIO_FILE="${MAISTRA_BRANCH}.zip"
-ISTIO_URL="https://github.com/Maistra/istio/archive/${MAISTRA_BRANCH}.zip"
-EXTRACT_CMD="unzip ${ISTIO_FILE} istio-${MAISTRA_BRANCH}/install/kubernetes/helm/*"
+ISTIO_URL="${MAISTRA_REPO}/archive/${MAISTRA_BRANCH}.zip"
+EXTRACT_DIR="${MAISTRA_REPO##*/}-${MAISTRA_BRANCH}"
+EXTRACT_CMD="unzip ${ISTIO_FILE} ${EXTRACT_DIR}/install/kubernetes/helm/*"
 RELEASE_DIR="${RELEASES_DIR}/${ISTIO_NAME}"
 
 ISTIO_NAME=${ISTIO_NAME//./-}
@@ -49,9 +51,9 @@ function retrieveIstioRelease() {
   (
       echo "extracting Istio Helm charts to ${RELEASES_DIR}"
       cd "${RELEASES_DIR}"
-      rm -rf istio-${MAISTRA_BRANCH}
+      rm -rf ${EXTRACT_DIR}
       ${EXTRACT_CMD}
-      cp -rf istio-${MAISTRA_BRANCH}/install/kubernetes/helm/* ${HELM_DIR}/
+      cp -rf ${EXTRACT_DIR}/install/kubernetes/helm/* ${HELM_DIR}/
       #(
       #  cd "${HELM_DIR}/istio"
       #  helm dep update
@@ -66,5 +68,5 @@ source $(dirname ${BASH_SOURCE})/patch-charts.sh
 (
   cd "${RELEASES_DIR}"
   echo "producing diff file for charts: $(pwd)/chart-diffs.diff"
-  diff -uNr istio-${MAISTRA_BRANCH}/install/kubernetes/helm/ ${HELM_DIR}/ > chart-diffs.diff || [ $? -eq 1 ]
+  diff -uNr ${EXTRACT_DIR}/install/kubernetes/helm/ ${HELM_DIR}/ > chart-diffs.diff || [ $? -eq 1 ]
 )
