02f25af4d0ed91a020bdda2f0e3484bdbdc7bf8f
Marko Lukša <marko.luksa@gmail.com>
2020-05-20 09:46:50 +0200

    MAISTRA-1427 Fix misleading message in condition of individual control plane components (#425)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/controller/servicemesh/controlplane/status.go
+++ pkg/controller/servicemesh/controlplane/status.go
@@ -4,20 +4,28 @@ import (
 	"github.com/maistra/istio-operator/pkg/apis/maistra/v1"
 )
 
-func updateReconcileStatus(status *v1.StatusType, err error) {
-	installStatus := status.GetCondition(v1.ConditionTypeInstalled).Status
+func updateControlPlaneConditions(status *v1.ControlPlaneStatus, err error) {
+	updateConditions(&status.StatusType, err, "Successfully installed all mesh components")
+}
+
+func updateComponentConditions(status *v1.ComponentStatus, err error) {
+	updateConditions(&status.StatusType, err, "Component installed successfully")
+}
+
+func updateConditions(status *v1.StatusType, err error, successfulInstallMessage string) {
+	installStatus := status.GetCondition(v1.ConditionTypeInstalled).Status // TODO: controller should never read the status to decide what to do
 	if err == nil {
 		if installStatus != v1.ConditionStatusTrue {
 			status.SetCondition(v1.Condition{
 				Type:    v1.ConditionTypeInstalled,
 				Reason:  v1.ConditionReasonInstallSuccessful,
-				Message: "Successfully installed all mesh components",
+				Message: successfulInstallMessage,
 				Status:  v1.ConditionStatusTrue,
 			})
 			status.SetCondition(v1.Condition{
 				Type:    v1.ConditionTypeReconciled,
 				Reason:  v1.ConditionReasonInstallSuccessful,
-				Message: "Successfully installed all mesh components",
+				Message: successfulInstallMessage,
 				Status:  v1.ConditionStatusTrue,
 			})
 		} else {
