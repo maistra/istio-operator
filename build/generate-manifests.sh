9ccb79b675d08edb2dd6f7c871b824636a8c4921
Kevin Conner <kconner@redhat.com>
2020-06-14 17:23:22 -0700

    MAISTRA-1534: Update operator metadata for 1.1.3

--- build/generate-manifests.sh
+++ build/generate-manifests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# to generate Maistra OLM metadata: MAISTRA_VERSION=1.0.1 REPLACES_CSV=1.0.0 build/generate-manifests.sh
-# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.0.1 REPLACES_CSV=1.0.0 build/generate-manifests.sh
+# to generate Maistra OLM metadata: COMMUNITY=true MAISTRA_VERSION=1.1.3 REPLACES_CSV=1.1.1 build/generate-manifests.sh
+# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.1.3 REPLACES_CSV=1.1.2+3 build/generate-manifests.sh
 
 set -e
 
