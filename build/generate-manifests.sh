e3fd0740ca111c3f838db935747ee87664e11abc
Kevin Conner <kconner@redhat.com>
2020-07-17 16:15:51 -0700

    MAISTRA-1557: Add service mesh icon and automate generation

--- build/generate-manifests.sh
+++ build/generate-manifests.sh
@@ -106,6 +106,7 @@ function generateCSV() {
      exit 1
   fi
 
+  ICON=$(cat ${MY_LOCATION}/manifest-templates/${BUILD_TYPE}_rgb_icon_default_128px.png | base64 | sed -e 's+^+      +')
   local csv_path=${BUNDLE_DIR}/${OPERATOR_NAME}.v${MAISTRA_VERSION}.clusterserviceversion.yaml
   cp ${MY_LOCATION}/manifest-templates/clusterserviceversion.yaml ${csv_path}
 
@@ -130,6 +131,10 @@ function generateCSV() {
   sed -i -e '/__DEPLOYMENT_SPEC__/{
     s/__DEPLOYMENT_SPEC__//
     r '<(echo "$DEPLOYMENT_SPEC")'
+  }' ${csv_path}
+  sed -i -e '/__ICON__/{
+    s/__ICON__//
+    r '<(echo "$ICON")'
   }' ${csv_path}
   if [ -z "$REPLACES_CSV" ]; then
     sed -i '/__REPLACES_CSV__/d' ${csv_path}
