077999f08b48d0f774455249fff66d567d6ac8fa
Kevin Conner <kconner@redhat.com>
2021-03-16 09:51:43 -0700

    MAISTRA-2160: Update for 1.1.13 release

--- build/generate-manifests.sh
+++ build/generate-manifests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# to generate Maistra OLM metadata: COMMUNITY=true MAISTRA_VERSION=1.1.12 REPLACES_CSV=1.1.11 build/generate-manifests.sh
-# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.1.12 REPLACES_CSV=1.1.11 build/generate-manifests.sh
+# to generate Maistra OLM metadata: COMMUNITY=true MAISTRA_VERSION=1.1.13 REPLACES_CSV=1.1.12 build/generate-manifests.sh
+# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.1.13 REPLACES_CSV=1.1.12 build/generate-manifests.sh
 
 set -e
 
