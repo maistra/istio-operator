2f81a73f5859ff5369135d2715d6bb08e607ded8
Kevin Conner <kconner@redhat.com>
2021-05-14 12:25:28 -0700

    MAISTRA-2359: Update metadata for 1.1.15 (#714)

--- Makefile
+++ Makefile
@@ -14,10 +14,10 @@
 
 -include Makefile.overrides
 
-MAISTRA_VERSION        ?= 1.1.14
+MAISTRA_VERSION        ?= 1.1.15
 MAISTRA_BRANCH         ?= maistra-1.1
-REPLACES_PRODUCT_CSV   ?= 1.1.13
-REPLACES_COMMUNITY_CSV ?= 1.1.13
+REPLACES_PRODUCT_CSV   ?= 1.1.14
+REPLACES_COMMUNITY_CSV ?= 1.1.14
 VERSION                ?= development
 IMAGE                  ?= docker.io/maistra/istio-ubi8-operator:${MAISTRA_VERSION}
 CONTAINER_CLI          ?= docker
--- build/generate-manifests.sh
+++ build/generate-manifests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# to generate Maistra OLM metadata: COMMUNITY=true MAISTRA_VERSION=1.1.14 REPLACES_CSV=1.1.13 build/generate-manifests.sh
-# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.1.14 REPLACES_CSV=1.1.13 build/generate-manifests.sh
+# to generate Maistra OLM metadata: COMMUNITY=true MAISTRA_VERSION=1.1.15 REPLACES_CSV=1.1.14 build/generate-manifests.sh
+# to generate ServiceMesh OLM metadata: COMMUNITY=false MAISTRA_VERSION=1.1.15 REPLACES_CSV=1.1.14 build/generate-manifests.sh
 
 set -e
 
