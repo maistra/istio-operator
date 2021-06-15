4da0f3f14d285b5a8b128f594382a1dc5b0a9654
Kevin Conner <kconner@redhat.com>
2021-06-15 15:03:43 -0700

    MAISTRA-2436: Update operator metadata for maistra 2.0.6

--- pkg/controller/servicemesh/controlplane/profiles_test.go
+++ pkg/controller/servicemesh/controlplane/profiles_test.go
@@ -16,9 +16,9 @@ import (
 )
 
 const (
-	PRODUCT_IMAGE_2_0   = "2.0.5"
-	COMMUNITY_IMAGE_2_0 = "2.0.5"
-	PRODUCT_IMAGE_1_1   = "1.1.15"
+	PRODUCT_IMAGE_2_0   = "2.0.6"
+	COMMUNITY_IMAGE_2_0 = "2.0.6"
+	PRODUCT_IMAGE_1_1   = "1.1.16"
 )
 
 func TestProfiles(t *testing.T) {
