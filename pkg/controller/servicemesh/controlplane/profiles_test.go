c84f3db4aa8d02aeb460e09af940852c5083ea42
Rob Cernich <rcernich@redhat.com>
2022-08-08 10:23:49 -0600

    OSSM-1866 OSSM-1867 update container versions to 2.0.11 and 2.1.5 (#964)
    Signed-off-by: rcernich <rcernich@redhat.com>

--- pkg/controller/servicemesh/controlplane/profiles_test.go
+++ pkg/controller/servicemesh/controlplane/profiles_test.go
@@ -17,10 +17,10 @@ import (
 
 const (
 	PRODUCT_IMAGE_1_1   = "1.1.18"
-	PRODUCT_IMAGE_2_0   = "2.0.10"
-	COMMUNITY_IMAGE_2_0 = "2.0.10"
-	COMMUNITY_IMAGE_2_1 = "2.1.4"
-	PRODUCT_IMAGE_2_1   = "2.1.4"
+	PRODUCT_IMAGE_2_0   = "2.0.11"
+	COMMUNITY_IMAGE_2_0 = "2.0.11"
+	COMMUNITY_IMAGE_2_1 = "2.1.5"
+	PRODUCT_IMAGE_2_1   = "2.1.5"
 )
 
 func TestProfiles(t *testing.T) {
