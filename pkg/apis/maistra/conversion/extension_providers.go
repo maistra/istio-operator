f8ccee1ecb8da7b241479e58b3b36c9e07f908d5
Jacek Ewertowski <jewertow@redhat.com>
2023-03-14 00:17:29 +0100

    Fix conversion of v2.SMCP to helm values
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- pkg/apis/maistra/conversion/extension_providers.go
+++ pkg/apis/maistra/conversion/extension_providers.go
@@ -2,7 +2,6 @@ package conversion
 
 import (
 	"fmt"
-
 	"github.com/maistra/istio-operator/pkg/apis/maistra/v1"
 	"github.com/maistra/istio-operator/pkg/apis/maistra/v2"
 )
@@ -12,36 +11,45 @@ func populateExtensionProvidersValues(in *v2.ControlPlaneSpec, values map[string
 		return nil
 	}
 
-	for i, ext := range in.ExtensionProviders {
+	for _, ext := range in.ExtensionProviders {
 		if ext.Prometheus == nil {
 			return fmt.Errorf("extension provider entry %s does not define any provider - it must specify one of: prometheus", ext.Name)
 		}
-		if err := setHelmStringValue(values, fmt.Sprintf("meshConfig.extensionProviders[%d].name", i), ext.Name); err != nil {
+		prometheus := []map[string]interface{}{
+			{
+				"name":       ext.Name,
+				"prometheus": map[string]interface{}{},
+			},
+		}
+		if err := setHelmMapSliceValue(values, "meshConfig.extensionProviders", prometheus); err != nil {
 			return err
 		}
-		if err := setHelmValue(values, fmt.Sprintf("meshConfig.extensionProviders[%d].prometheus", i), ext.Prometheus); err != nil {
-			return err
-		}
+		//if err := setHelmStringValue(values, fmt.Sprintf("meshConfig.extensionProviders[%d].name", i), ext.Name); err != nil {
+		//	return err
+		//}
+		//if err := setHelmValue(values, fmt.Sprintf("meshConfig.extensionProviders[%d].prometheus", i), ext.Prometheus); err != nil {
+		//	return err
+		//}
 	}
 	return nil
 }
 
 func populateExtensionProvidersConfig(in *v1.HelmValues, out *v2.ControlPlaneSpec) error {
-	rawMeshConfigValues, ok, err := in.GetMap("meshConfig")
-	if err != nil {
-		return err
-	}
-	if !ok || len(rawMeshConfigValues) == 0 {
-		return nil
-	}
-
-	meshConfigValues := v1.NewHelmValues(rawMeshConfigValues)
-	if rawExtProviders, ok, err := meshConfigValues.GetAndRemoveSlice("extensionProviders"); ok {
-		for _, rawExtProvider := range rawExtProviders {
-			if extProvider, ok := rawExtProvider.(*v2.ExtensionProviderConfig); ok {
-				out.ExtensionProviders = append(out.ExtensionProviders, extProvider)
-			}
-		}
+	//rawMeshConfigValues, ok, err := in.GetMap("meshConfig")
+	//if err != nil {
+	//	return err
+	//}
+	//if !ok || len(rawMeshConfigValues) == 0 {
+	//	return nil
+	//}
+	//
+	//meshConfigValues := v1.NewHelmValues(rawMeshConfigValues)
+	if _, ok, err := in.GetAndRemoveSlice("meshConfig.extensionProviders"); ok {
+		//for _, rawExtProvider := range rawExtProviders {
+		//	if extProvider, ok := rawExtProvider.(*v2.ExtensionProviderConfig); ok {
+		//		out.ExtensionProviders = append(out.ExtensionProviders, extProvider)
+		//	}
+		//}
 	} else if err != nil {
 		return err
 	}
