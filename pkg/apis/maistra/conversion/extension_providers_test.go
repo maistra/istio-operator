687a0b6e949fa73b5aa63b1cb2e5ec59b90a863d
Jacek Ewertowski <jewertow@redhat.com>
2023-03-14 16:50:57 +0100

    Try to fix lint errors
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- pkg/apis/maistra/conversion/extension_providers_test.go
+++ pkg/apis/maistra/conversion/extension_providers_test.go
@@ -4,8 +4,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/maistra/istio-operator/pkg/apis/maistra/v1"
-	"github.com/maistra/istio-operator/pkg/apis/maistra/v2"
+	v1 "github.com/maistra/istio-operator/pkg/apis/maistra/v1"
+	v2 "github.com/maistra/istio-operator/pkg/apis/maistra/v2"
 	"github.com/maistra/istio-operator/pkg/controller/versions"
 )
 
