b0fbba7bfe30fe6448663b1cdcf93d55062b381a
Marko Lukša <marko.luksa@gmail.com>
2023-09-12 10:17:35 +0200

    OSSM-4175 Install CRDs together with the operator

--- hack/download-charts.sh
+++ hack/download-charts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e -u
+set -e -u -o pipefail
 
 : "${MAISTRA_RELEASE_STREAM:=$1}"
 : "${ISTIO_REPO:=$2}"
@@ -9,6 +9,7 @@ set -e -u
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT=$(dirname "${SCRIPT_DIR}")
 HELM_DIR="${REPO_ROOT}/resources/charts/${MAISTRA_RELEASE_STREAM}"
+CONFIG_DIR="${REPO_ROOT}/config"
 
 ISTIO_FILE="${ISTIO_COMMIT}.tar.gz"
 ISTIO_URL="${ISTIO_REPO}/archive/${ISTIO_COMMIT}.tar.gz"
@@ -40,5 +41,36 @@ function patchIstioCharts() {
   verbs: ["use"]/' "${HELM_DIR}/istio-cni/templates/clusterrole.yaml"
 }
 
+function copyCRDs() {
+  # Split the YAML file into separate CRD files
+  csplit -s --suppress-matched -f "${CONFIG_DIR}/crd/bases/istio-crd" -z "${HELM_DIR}/base/crds/crd-all.gen.yaml" '/^---$/' '{*}'
+
+  # Rename the split files to <api group>_<resource name>.yaml
+  for file in "${CONFIG_DIR}/crd/bases/istio-crd"*; do
+    # Extract the group and resource from each CRD
+    group=$(grep -oP '^\s*group:\s*\K.*' "$file" | tr -d '[:space:]')
+    resource=$(grep -oP '^\s*plural:\s*\K.*' "$file" | tr -d '[:space:]')
+
+    # Rename the file to <group>_<resource>.yaml
+    mv "$file" "${CONFIG_DIR}/crd/bases/${group}_${resource}.yaml"
+  done
+
+  # Remove existing list of CRD files from kustomization.yaml
+  sed -i '/resources:/,/#+kubebuilder:scaffold:crdkustomizeresource/ {
+    /resources:/n
+    /#+kubebuilder:scaffold:crdkustomizeresource/!d
+    }' "${CONFIG_DIR}/crd/kustomization.yaml"
+
+  # Create YAML snippet containing list of CRD files
+  { cd "${CONFIG_DIR}/crd"; find "bases/"*.yaml | sed 's/^/- /'; } > "${CONFIG_DIR}/crd/crdfiles.yaml"
+
+  # Insert snippet into kustomization.yaml
+  sed -i '/resources:/r '"${CONFIG_DIR}/crd/crdfiles.yaml" "${CONFIG_DIR}/crd/kustomization.yaml"
+
+  # Remove snippet file
+  rm "${CONFIG_DIR}/crd/crdfiles.yaml"
+}
+
 downloadIstioCharts
 patchIstioCharts
+copyCRDs
