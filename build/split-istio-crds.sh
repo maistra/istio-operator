687c149d5f5aef2eefff9cc7966ca0174b5d0870
Francisco Herrera Lira <fjglira@gmail.com>
2023-09-22 11:37:31 +0200

    OSSM-4855 Adding build operator support for MacOs (#1314)
    Co-authored-by: Francisco Herrera <frherrer@redhat.com>

--- build/split-istio-crds.sh
+++ build/split-istio-crds.sh
@@ -5,7 +5,8 @@ set -e
 : ${CRD_DIR:?"Need to set CRD_DIR to location of CRD yaml files, e.g. resources/helm/v1.0/istio-init/files"}
 : ${SOURCE_DIR:=$(pwd)}
 
-CRD_DIR=$(realpath --relative-to "${SOURCE_DIR}" "${CRD_DIR}")
+CRD_DIR="${CRD_DIR#${SOURCE_DIR}/}"
+
 (
   cd "${SOURCE_DIR}"
   find "${CRD_DIR}" -maxdepth 1 -name "*.yaml" -type f ! -name "*crd.yaml" | xargs -t go run -mod=vendor ./tools/crd --zap-encoder=console
