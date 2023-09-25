2d2d89d74c86f3fa78bd38b79a290041ff95dd32
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-09-25 15:19:02 +0000

    [maistra-2.4] OSSM-4855 Add support to build operator on MacOs (#1322)
    * OSSM-4855 Add support to build operator on MacOs
    
    * Update build/generate-manifests.sh
    
    Co-authored-by: Marko Lukša <mluksa@redhat.com>
    
    * Update build/split-istio-crds.sh
    
    Co-authored-by: Marko Lukša <mluksa@redhat.com>
    
    ---------
    
    Co-authored-by: Francisco Herrera <frherrer@redhat.com>
    Co-authored-by: Francisco Herrera Lira <fjglira@gmail.com>
    Co-authored-by: Marko Lukša <mluksa@redhat.com>

--- build/split-istio-crds.sh
+++ build/split-istio-crds.sh
@@ -5,7 +5,8 @@ set -e -u
 : "${CRD_DIR:?"Need to set CRD_DIR to location of CRD yaml files, e.g. resources/helm/v2.0/istio-init/files"}"
 : "${SOURCE_DIR:=$(pwd)}"
 
-CRD_DIR=$(realpath --relative-to "${SOURCE_DIR}" "${CRD_DIR}")
+CRD_DIR="${CRD_DIR#"${SOURCE_DIR}/"}"
+
 (
   cd "${SOURCE_DIR}"
   find "${CRD_DIR}" -maxdepth 1 -name "*.yaml" -type f ! -name "*crd.yaml" -exec go run ./tools/crd --zap-encoder=console {} +
