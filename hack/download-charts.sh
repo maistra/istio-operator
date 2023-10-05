1180bd176fb928ac34584185152c0ad7d45c01bf
Marko Lukša <marko.luksa@gmail.com>
2023-10-05 12:19:35 +0200

    OSSM-4942 Hide istio CRDs in the OpenShift Console

--- hack/download-charts.sh
+++ hack/download-charts.sh
@@ -45,12 +45,16 @@ function copyCRDs() {
   # Split the YAML file into separate CRD files
   csplit -s --suppress-matched -f "${CONFIG_DIR}/crd/bases/istio-crd" -z "${HELM_DIR}/base/crds/crd-all.gen.yaml" '/^---$/' '{*}'
 
+  # To hide istio CRDs in the OpenShift Console, we add them to the intenral-objects annotation in the CSV
+  internalObjects=""
+
   # Rename the split files to <api group>_<resource name>.yaml
   for file in "${CONFIG_DIR}/crd/bases/istio-crd"*; do
     # Extract the group and resource from each CRD
     group=$(grep -oP '^\s*group:\s*\K.*' "$file" | tr -d '[:space:]')
     resource=$(grep -oP '^\s*plural:\s*\K.*' "$file" | tr -d '[:space:]')
-
+    # Add the CRD to the list of internal objects
+    internalObjects+="\"${resource}.${group}\","
     # Rename the file to <group>_<resource>.yaml
     mv "$file" "${CONFIG_DIR}/crd/bases/${group}_${resource}.yaml"
   done
@@ -69,6 +73,9 @@ function copyCRDs() {
 
   # Remove snippet file
   rm "${CONFIG_DIR}/crd/crdfiles.yaml"
+
+  # Update internal-objects annotation in CSV
+  sed -i "/operators\.operatorframework\.io\/internal-objects/ c\    operators.operatorframework.io/internal-objects: '[${internalObjects%?}]'" "${CONFIG_DIR}/manifests/bases/sailoperator.clusterserviceversion.yaml"
 }
 
 downloadIstioCharts
