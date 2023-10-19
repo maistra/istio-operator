b13ce2e57191b0a8085a4789891e9cd8885dd01b
Jonh Wendell <jwendell@redhat.com>
2023-10-19 16:03:36 -0400

    Add kube-rbac-proxy to the update_deps script
    Plus fix some issues with spaces and minor makefile enhancements.

--- tools/update_deps.sh
+++ tools/update_deps.sh
@@ -25,19 +25,23 @@ go mod tidy
 
 # Update operator-sdk
 OPERATOR_SDK_LATEST_VERSION=$(getLatestVersion operator-framework/operator-sdk)
-sed -i "s|OPERATOR_SDK_VERSION ?= .*| OPERATOR_SDK_VERSION ?= ${OPERATOR_SDK_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+sed -i "s|OPERATOR_SDK_VERSION ?= .*|OPERATOR_SDK_VERSION ?= ${OPERATOR_SDK_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update kustomize
 KUSTOMIZE_LATEST_VERSION=$(getLatestVersion kubernetes-sigs/kustomize | cut -d/ -f2)
-sed -i "s|KUSTOMIZE_VERSION ?= .*| KUSTOMIZE_VERSION ?= ${KUSTOMIZE_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+sed -i "s|KUSTOMIZE_VERSION ?= .*|KUSTOMIZE_VERSION ?= ${KUSTOMIZE_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update controller-tools
 CONTROLLER_TOOLS_LATEST_VERSION=$(getLatestVersion kubernetes-sigs/controller-tools)
-sed -i "s|CONTROLLER_TOOLS_VERSION ?= .*| CONTROLLER_TOOLS_VERSION ?= ${CONTROLLER_TOOLS_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+sed -i "s|CONTROLLER_TOOLS_VERSION ?= .*|CONTROLLER_TOOLS_VERSION ?= ${CONTROLLER_TOOLS_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
 
 # Update opm
 OPM_LATEST_VERSION=$(getLatestVersion operator-framework/operator-registry)
-sed -i "s|OPM_VERSION ?= .*| OPM_VERSION ?= ${OPM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+sed -i "s|OPM_VERSION ?= .*|OPM_VERSION ?= ${OPM_LATEST_VERSION}|" "${ROOTDIR}/Makefile.core.mk"
+
+# Update kube-rbac-proxy
+RBAC_PROXY_LATEST_VERSION=$(getLatestVersion brancz/kube-rbac-proxy | cut -d/ -f1)
+sed -i "s|gcr.io/kubebuilder/kube-rbac-proxy:.*|gcr.io/kubebuilder/kube-rbac-proxy:${RBAC_PROXY_LATEST_VERSION}|" "${ROOTDIR}/config/default/manager_auth_proxy_patch.yaml"
 
 # Regenerate files
 make gen
