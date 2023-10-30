173c5ff4730d08da7a06e69c41afa7af5b349947
Jonh Wendell <jwendell@redhat.com>
2023-10-30 12:12:24 -0400

    Wait until artifacts are available on GCS

--- hack/update-istio.sh
+++ hack/update-istio.sh
@@ -2,14 +2,31 @@
 
 set -euo pipefail
 
-COMMIT=$(yq eval '"git ls-remote --heads " + .latest.repo + ".git " + .latest.branch + " | cut -f 1"' versions.yaml | sh)
-echo Updating version 'latest' to commit "${COMMIT}"
+SLEEP_TIME=60
 
-FULL_VERSION=$(curl -s "https://storage.googleapis.com/istio-build/dev/$COMMIT")
+COMMIT=$(yq eval '"git ls-remote --heads " + .latest.repo + ".git " + .latest.branch + " | cut -f 1"' versions.yaml | sh)
+CURRENT=$(yq .latest.commit versions.yaml)
+
+if [ "${COMMIT}" == "${CURRENT}" ]; then
+  echo "versions.yaml is already up-to-date with latest commit ${COMMIT}."
+  exit 0
+fi
+
+echo Updating version 'latest' to commit "${COMMIT}"
+echo "Verifying the artifacts are available on GCS, this might take a while - you can abort the wait with CTRL+C"
+
+URL="https://storage.googleapis.com/istio-build/dev/${COMMIT}"
+until curl --output /dev/null --silent --head --fail "${URL}"; do
+    echo -n '.'
+    sleep ${SLEEP_TIME}
+done
+echo
+
+FULL_VERSION=$(curl -sSfL "${URL}")
 echo Full version: "${FULL_VERSION}"
 
 yq -i '
-    .latest.commit="'"$COMMIT"'" |
+    .latest.commit="'"${COMMIT}"'" |
     .latest.charts=[
         "https://storage.googleapis.com/istio-build/dev/'"${FULL_VERSION}"'/helm/base-'"${FULL_VERSION}"'.tgz",
         "https://storage.googleapis.com/istio-build/dev/'"${FULL_VERSION}"'/helm/cni-'"${FULL_VERSION}"'.tgz",
