b6be05f5b518261042e734606fa6a6c9290201fe
Marko Lukša <marko.luksa@gmail.com>
2023-10-30 14:17:49 +0100

    Get latest charts from storage.googleapis.com instead of github

--- hack/update-istio.sh
+++ hack/update-istio.sh
@@ -3,6 +3,17 @@
 set -euo pipefail
 
 COMMIT=$(yq eval '"git ls-remote --heads " + .latest.repo + ".git " + .latest.branch + " | cut -f 1"' versions.yaml | sh)
-echo Updating version 'latest' to commit ${COMMIT}
+echo Updating version 'latest' to commit "${COMMIT}"
 
-yq -i '.latest.commit="'${COMMIT}'"' versions.yaml
+FULL_VERSION=$(curl -s "https://storage.googleapis.com/istio-build/dev/$COMMIT")
+echo Full version: "${FULL_VERSION}"
+
+yq -i '
+    .latest.commit="'"$COMMIT"'" |
+    .latest.charts=[
+        "https://storage.googleapis.com/istio-build/dev/'"${FULL_VERSION}"'/helm/base-'"${FULL_VERSION}"'.tgz",
+        "https://storage.googleapis.com/istio-build/dev/'"${FULL_VERSION}"'/helm/cni-'"${FULL_VERSION}"'.tgz",
+        "https://storage.googleapis.com/istio-build/dev/'"${FULL_VERSION}"'/helm/gateway-'"${FULL_VERSION}"'.tgz",
+        "https://storage.googleapis.com/istio-build/dev/'"${FULL_VERSION}"'/helm/istiod-'"${FULL_VERSION}"'.tgz",
+        "https://storage.googleapis.com/istio-build/dev/'"${FULL_VERSION}"'/helm/ztunnel-'"${FULL_VERSION}"'.tgz"
+    ]' versions.yaml
