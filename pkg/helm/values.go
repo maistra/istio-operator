13f58df204ffb9bd52ed0a42534a4a2eb6b723b5
Marko Lukša <marko.luksa@gmail.com>
2023-11-07 14:35:46 +0100

    Add comments and fix argument type

--- pkg/helm/values.go
+++ pkg/helm/values.go
@@ -8,11 +8,15 @@ import (
 
 type HelmValues map[string]interface{}
 
+// GetString returns the string value of a nested field.
+// Returns false if value is not found and an error if not a string.
 func (h HelmValues) GetString(key string) (string, bool, error) {
 	return unstructured.NestedString(h, toKeys(key)...)
 }
 
-func (h HelmValues) Set(key string, val string) error {
+// Set sets the value of a nested field to a deep copy of the value provided.
+// Returns an error if value cannot be set because one of the nesting levels is not a map[string]interface{}.
+func (h HelmValues) Set(key string, val interface{}) error {
 	return unstructured.SetNestedField(h, val, toKeys(key)...)
 }
 
