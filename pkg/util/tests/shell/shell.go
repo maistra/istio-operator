a4d6cdc2f8947ad254627c943de8e91a062deb98
Francisco Herrera <frherrer@redhat.com>
2024-03-08 16:51:07 +0000

    More changes from review

--- pkg/util/tests/shell/shell.go
+++ pkg/util/tests/shell/shell.go
@@ -22,30 +22,21 @@ import (
 	"strings"
 )
 
-// ExecuteCommandWithFile executes a command given the input string and returns the output and err if any
-func ExecuteCommandWithFile(command string, filename string) (string, error) {
-	return ExecuteShell(command, "", filename)
-}
-
 // ExecuteCommandWithInput executes a command given the input string and returns the output and err if any
 func ExecuteCommandWithInput(command string, input string) (string, error) {
-	return ExecuteShell(command, input, "")
+	return ExecuteShell(command, input)
 }
 
 // ExecuteCommandWithInput executes a command given the input string and returns the output and err if any
 func ExecuteCommand(command string) (string, error) {
-	return ExecuteShell(command, "", "")
+	return ExecuteShell(command, "")
 }
 
 // ExecuteCommand executes a command given the input string and returns the output and err if any
-func ExecuteShell(command string, input string, filename string) (string, error) {
+func ExecuteShell(command string, input string) (string, error) {
 	cmd := exec.Command("sh", "-c", command)
 	if input != "" {
 		cmd.Stdin = strings.NewReader(input)
-	}
-	if filename != "" {
-		cmd = exec.Command("sh", "-c", command, filename)
-		fmt.Println("Command: ", command, filename)
 	}
 
 	var stdout bytes.Buffer
@@ -57,7 +48,7 @@ func ExecuteShell(command string, input string, filename string) (string, error)
 	err := cmd.Run()
 	if err != nil {
 		// Return both stdout and stderr to help with debugging
-		return stdout.String() + stderr.String(), fmt.Errorf("error executing command: %v, output: %s", err, stderr.String())
+		return stdout.String() + stderr.String(), fmt.Errorf("error executing command: %s", stderr.String())
 	}
 
 	return stdout.String(), nil
