ae826c0e641ea807f52a900feea8a6b4d6cc04ab
Francisco Herrera <fjglira@gmail.com>
2024-03-21 13:18:56 +0100

    OSSM-6132 Split make test targets (#1711)
    * OSSM-6132 Split make test targets
    
    * update test make target from review
    
    * Improve the test filter in make target
    
    * Apply suggestions from code review
    
    Co-authored-by: Marko Lukša <mluksa@redhat.com>
    
    ---------
    
    Co-authored-by: Marko Lukša <mluksa@redhat.com>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -143,9 +143,16 @@ export
 ##@ Testing
 
 .PHONY: test
-test: envtest ## Run unit tests.
-	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) --bin-dir $(LOCALBIN) -p path)" \
+test: test.unit test.integration ## Run both unit tests and integration test.
+
+.PHONY: test.unit
+test.unit:  ## Run unit tests.
 	go test $(VERBOSE_FLAG) ./... -coverprofile cover.out
+
+.PHONY: test.integration
+test.integration: envtest ## Run integration tests located in the tests/integration directory.
+	KUBEBUILDER_ASSETS="$(shell $(ENVTEST) use $(ENVTEST_K8S_VERSION) --bin-dir $(LOCALBIN) -p path)" \
+	go test --tags=integration $(VERBOSE_FLAG) ./tests/integration/... -coverprofile cover.out
 
 .PHONY: test.scorecard
 test.scorecard: operator-sdk ## Run the operator scorecard test.
