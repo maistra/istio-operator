623cf34940fd3d5e43843a7715558849e8ddb854
frherrer <frherrer@redhat.com>
2024-03-27 18:08:45 +0000

    Update to be able to run the go client against OCP clusters

--- tests/e2e/common-operator-integ-suite.sh
+++ tests/e2e/common-operator-integ-suite.sh
@@ -80,6 +80,7 @@ initialize_variables() {
   CONTROL_PLANE_NS="${CONTROL_PLANE_NS:-istio-system}"
   COMMAND="kubectl"
   ARTIFACTS="${ARTIFACTS:-$(mktemp -d)}"
+  KUBECONFIG="${KUBECONFIG:-"${ARTIFACTS}/config"}"
 
   if [ "${OCP}" == "true" ]; then
     COMMAND="oc"
@@ -211,5 +212,5 @@ fi
 # Run the go test passing the env variables defined that are going to be used in the operator tests
 IMAGE="${HUB}/${IMAGE_BASE}:${TAG}" SKIP_DEPLOY="${SKIP_DEPLOY}" OCP="${OCP}" ISTIO_MANIFEST="${ISTIO_MANIFEST}" \
 NAMESPACE="${NAMESPACE}" CONTROL_PLANE_NS="${CONTROL_PLANE_NS}" DEPLOYMENT_NAME="${DEPLOYMENT_NAME}" \
-ISTIO_NAME="${ISTIO_NAME}" COMMAND="${COMMAND}" VERSIONS_YAML_FILE="${VERSIONS_YAML_FILE}" KUBECONFIG="${ARTIFACTS}/config" \
+ISTIO_NAME="${ISTIO_NAME}" COMMAND="${COMMAND}" VERSIONS_YAML_FILE="${VERSIONS_YAML_FILE}" KUBECONFIG="${KUBECONFIG}" \
 go run github.com/onsi/ginkgo/v2/ginkgo -tags e2e "${VERBOSE_FLAG}" --timeout 30m --junit-report=report.xml "${NOCOLOR}" "${WD}"/operator/...
