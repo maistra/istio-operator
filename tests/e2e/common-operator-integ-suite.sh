3c729169f387de7513fc233be20bbc51bcb2c762
Marko Lukša <marko.luksa@gmail.com>
2024-03-29 07:36:57 +0100

    Fix --no-color in e2e tests

--- tests/e2e/common-operator-integ-suite.sh
+++ tests/e2e/common-operator-integ-suite.sh
@@ -196,7 +196,8 @@ if [ "${OCP}" == "true" ]; then
 fi
 
 # Run the go test passing the env variables defined that are going to be used in the operator tests
+# shellcheck disable=SC2086
 IMAGE="${HUB}/${IMAGE_BASE}:${TAG}" SKIP_DEPLOY="${SKIP_DEPLOY}" OCP="${OCP}" ISTIO_MANIFEST="${ISTIO_MANIFEST}" \
 NAMESPACE="${NAMESPACE}" CONTROL_PLANE_NS="${CONTROL_PLANE_NS}" DEPLOYMENT_NAME="${DEPLOYMENT_NAME}" \
 ISTIO_NAME="${ISTIO_NAME}" COMMAND="${COMMAND}" VERSIONS_YAML_FILE="${VERSIONS_YAML_FILE}" \
-go run github.com/onsi/ginkgo/v2/ginkgo -tags e2e --timeout 30m --junit-report=report.xml "${GINKGO_FLAGS}" "${WD}"/operator/...
+go run github.com/onsi/ginkgo/v2/ginkgo -tags e2e --timeout 30m --junit-report=report.xml ${GINKGO_FLAGS} "${WD}"/operator/...
